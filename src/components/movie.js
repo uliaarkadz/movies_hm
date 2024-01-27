b7fce917b4e3712439bdc503bda42e3c522fe016
ybuiko <juliabuiko@gmail.com>
2024-01-26 21:11:38 -0500

    project cretaed, added data, movie component

---
+++ src/components/movie.js
@@ -0,0 +1,7 @@
+const Movie = (props) => {
+  //The Components Returned JSX
+  return <div className="movie"> This is Movie</div>;
+};
+
+// export the component
+export default Movie;
---
+++ src/movies.js
@@ -0,0 +1,34 @@
+const movies = [
+  {
+    name: "xxxxxxxxxxxxxxxxx",
+    year: "2222",
+    rating: "G",
+    image: "https://www.something.com/image.png",
+  },
+  {
+    name: "xxxxxxxxxxxxxxxxx",
+    year: "2222",
+    rating: "G",
+    image: "https://www.something.com/image.png",
+  },
+  {
+    name: "xxxxxxxxxxxxxxxxx",
+    year: "2222",
+    rating: "G",
+    image: "https://www.something.com/image.png",
+  },
+  {
+    name: "xxxxxxxxxxxxxxxxx",
+    year: "2222",
+    rating: "G",
+    image: "https://www.something.com/image.png",
+  },
+  {
+    name: "xxxxxxxxxxxxxxxxx",
+    year: "2222",
+    rating: "G",
+    image: "https://www.something.com/image.png",
+  },
+];
+
+export default movies;
